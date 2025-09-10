bb2aad531db79256925c9d37871f75d1cb4203b7
Mai Thành <150693168+Thanhuoa@users.noreply.github.com>
2025-09-10 09:07:36 +0700

    Add files via upload

---
+++ Session 3/Ex5.cpp
@@ -0,0 +1,41 @@
+/* Ex5. The char rectangle 5
+Input                   Output
+4 4                     DDDD
+                        CDDD
+                        BCDD
+                        ABCD
+----------------------------
+3 5                     CDEEE
+                        BCDEE
+                        ABCDE
+-----------------------------
+5 3                     CCC
+                        CCC
+                        CCC
+                        BCC
+                        ABC
+*/
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int min(int a, int b) {
+    return a<b?a:b;
+}
+
+int main() {
+    int a,b;
+    cout << "Enter a,b: ";
+    cin >> a >> b;
+
+    for(int i=a; i>0; i--) {
+        char x='A'+min(i,b)-1;
+        for(int j=1; j<=b; j++) {
+            if(j<=b-i) cout << x++;
+            else cout << x;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
